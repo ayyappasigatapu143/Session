a56e63fe9aa61abb4ff8a7ea9ffb538f9076d8ca
ayappasigatapu143 <ayappa.sigatapu143gmail.com>
2020-04-17 00:17:16 +0530

    asgasga

--- server/src/main/java/com/info/beans/config/EmployeeConfig.java
+++ server/src/main/java/com/info/beans/config/EmployeeConfig.java
@@ -22,7 +22,13 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.google.common.base.Preconditions;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2; 
 
+@EnableSwagger2
 @Configuration
 @EnableTransactionManagement
 @PropertySource("classpath:application.properties")
@@ -80,4 +86,13 @@ public class EmployeeConfig {
 
 		return properties;
 	}
+	
+	@Bean
+	    public Docket api() { 
+	        return new Docket(DocumentationType.SWAGGER_2)  
+	          .select()                                  
+	          .apis(RequestHandlerSelectors.any())              
+	          .paths(PathSelectors.any())                          
+	          .build();                                           
+	    }
 }
